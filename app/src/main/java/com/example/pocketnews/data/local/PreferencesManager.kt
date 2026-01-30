e0ce962bdd19d5336ce02d4ab161e69a81a07346
helinertgrl <helinertgrll@gmail.com>
2026-01-30 21:24:01 +0300

    feat: add PreferencesManager and Hilt AppModule for data persistence

---
+++ app/src/main/java/com/example/pocketnews/data/local/PreferencesManager.kt
@@ -0,0 +1,52 @@
+package com.example.pocketnews.data.local
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore(name = "settings")
+
+class PreferencesManager(private val context: Context) {
+
+    companion object{
+        private val CATEGORY_KEY = stringPreferencesKey("selected_category")
+        private val UPDATE_INTERVAL_HOURS = intPreferencesKey("update_interval")
+        private val IS_FIRST_LAUNCH = booleanPreferencesKey("is_first_launch")
+    }
+
+
+    suspend fun saveCategoryToDataStore(categoryName: String){
+        context.dataStore.edit { settings ->
+            settings[CATEGORY_KEY] = categoryName
+        }
+    }
+
+    suspend fun saveHoursToDataStore(updateHours: Int){
+        context.dataStore.edit { settings ->
+            settings[UPDATE_INTERVAL_HOURS] = updateHours
+        }
+    }
+
+    suspend fun saveLaunchToDataStore(launch: Boolean){
+        context.dataStore.edit { settings ->
+            settings[IS_FIRST_LAUNCH] = launch
+        }
+    }
+
+    val categoryFlow: Flow<String> = context.dataStore.data.map { settings ->
+        settings[CATEGORY_KEY] ?: "General"
+    }
+
+    val updatehoursflow: Flow<Int> = context.dataStore.data.map { settings ->
+        settings[UPDATE_INTERVAL_HOURS] ?: 1
+    }
+
+    val isfirstlaunchflow: Flow<Boolean> = context.dataStore.data.map { settings ->
+        settings[IS_FIRST_LAUNCH] ?: true
+    }
+}
---
+++ app/src/main/java/com/example/pocketnews/di/AppModule.kt
@@ -0,0 +1,20 @@
+package com.example.pocketnews.di
+
+import android.content.Context
+import com.example.pocketnews.data.local.PreferencesManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun providePreferencesManager(@ApplicationContext context: Context): PreferencesManager{
+        return PreferencesManager(context)
+    }
+}
