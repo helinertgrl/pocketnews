baa12d0c4f09bed2856d3354529d982f59b46c60
helinertgrl <helinertgrll@gmail.com>
2026-02-05 22:51:12 +0300

    feat: Implement NewsCheckWorker with Hilt injection and background sync logic * Category control using DataStore has been added. * New news control provided with API and DB date comparison. * Try-catch and retry mechanism established for error management.

---
+++ app/src/main/java/com/example/pocketnews/worker/NewsCheckWorker.kt
@@ -0,0 +1,57 @@
+package com.example.pocketnews.worker
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.pocketnews.data.local.NewsArticleEntity
+import com.example.pocketnews.data.local.NewsDao
+import com.example.pocketnews.data.local.PreferencesManager
+import com.example.pocketnews.data.remote.NewsApiService
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.first
+
+@HiltWorker
+class NewsCheckWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val preferencesManager: PreferencesManager,
+    private val newsDao: NewsDao,
+    private val apiService: NewsApiService
+    ): CoroutineWorker(context,workerParams){
+
+    override suspend fun doWork(): Result {
+        try {
+            val selectedCategory = preferencesManager.categoryFlow.first()
+            val lastDateInDb = newsDao.getLatestPublishDate(selectedCategory)
+            val response = apiService.getNewsApiService("tr",selectedCategory,"62a0b36c61884473973acd7d1cf95fc2")
+            val latestNewsFromApi = response.articles.firstOrNull()
+
+            if (latestNewsFromApi != null && latestNewsFromApi.publishedAt > (lastDateInDb ?:"")){
+
+                val newsToInsert = response.articles.map { article ->
+
+                    NewsArticleEntity(
+                        title = article.title,
+                        description = article.description.orEmpty(),
+                        url = article.url,
+                        publishedAt = article.publishedAt,
+                        category = selectedCategory,
+                        source = article.source.name,
+                        isRead = false
+                    )
+                }
+                newsDao.insertArticles(newsToInsert)
+                println("Yeni haberler veritabanına başarıyla kaydedildi.")
+                //TODO: FIREBASE BİLDİRİMİ BURAYA GLECEK
+
+            }
+            return Result.success()
+
+        }catch (e: Exception){
+            println("Hata oluştu: ${e.localizedMessage}")
+            return Result.retry()
+        }
+    }
+}
