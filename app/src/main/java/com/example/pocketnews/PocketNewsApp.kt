4f7c6923b3ea08599dbb1d5a31039aa2182cb449
helinertgrl <helinertgrll@gmail.com>
2026-02-06 15:10:31 +0300

    chore: configure HiltWorkerFactory and WorkManager initialization * Implemented Configuration.Provider in Application class. * Injected HiltWorkerFactory for background dependency management. * Disabled default WorkManager initializer in AndroidManifest.

--- app/src/main/java/com/example/pocketnews/PocketNewsApp.kt
+++ app/src/main/java/com/example/pocketnews/PocketNewsApp.kt
@@ -1,7 +1,20 @@
 package com.example.pocketnews
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class PocketNewsApp : Application()
+class PocketNewsApp : Application(), Configuration.Provider{
+
+    @Inject
+    lateinit var  workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
+}
